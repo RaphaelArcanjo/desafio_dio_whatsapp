5da83ac0257e73590e2eb34f0fa12d70910bc1dc
Raphael Oliveira Cunha <raphael.cunha.br@gmail.com>
2026-09-02 17:53:03 -0300

    Ajustes para ficar de acordo com o rubocop

--- main.rb
+++ main.rb
@@ -1,18 +1,19 @@
-def validar_whatsapp whatsapp
-    if /\([0-9]{2}\) [0-9] [0-9]{4}\-[0-9]{4}/.match(whatsapp)
-        puts "Formato válido!"
-    else
-        puts "O formato do WhatsApp digitado é inválido!"
-    end
+# frozen_string_literal: true
+
+def validar_whatsapp(whatsapp)
+  if /\([0-9]{2}\) [0-9] [0-9]{4}-[0-9]{4}/.match(whatsapp)
+    puts 'Formato válido!'
+  else
+    puts 'O formato do WhatsApp digitado é inválido!'
+  end
 end
 
 def obter_whatsapp
-    puts "Digite o seu WhatsApp no formato (99) 9 9999-9999"
-    whatsapp = gets.chomp
-    return whatsapp
+  puts 'Digite o seu WhatsApp no formato (99) 9 9999-9999'
+  gets.chomp
 end
 
 def run
-    whatsapp = obter_whatsapp
-    validar_whatsapp whatsapp
+  whatsapp = obter_whatsapp
+  validar_whatsapp whatsapp
 end
